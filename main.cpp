a1a51eef1afbad650afe3e2e3f5bddab60381d51
dobidugi <dobidugi@gmail.com>
2017-04-22 14:08:26 +0900

    final1.1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Windows.h>
+//#include "board.h"
 #include "omok.h"
 
 int main()
@@ -7,13 +8,19 @@ int main()
 	COORD Pos;
 	Pos.X = 0;
 	Pos.Y = 22;
-	Omok *M = new Omok;
-	M->start();
+	int getx, gety;
+	std::cout << "input widthsize : ";
+	std::cin >> getx; 
+	std::cout << "input heightsize : ";
+	std::cin >> gety;
+	Omok *M = new Omok(getx, gety);
+	//M->start();
 	while (M->set()==1)
 	{
-		M->move();;
+		M->move();
 	}
 	Sleep(3000);
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 	delete M;
+	
 }
--- omok.cpp
+++ omok.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <vector>
 #include <Windows.h>
 #include <conio.h>
 #include "omok.h"
 
-#define _X 20
-#define _Y 20 
+//#define _X 20
+//#define _Y 20 
 
 // left ¦§ 1
 // right ¦© 2
@@ -19,30 +20,81 @@
 
 // ¡Û black 8
 // ¡Ü white 9
+
+
+Omok::Omok(int _x, int _y) : loop_x(_x) , loop_y(_y) , x(_x),y(_y)
+{
+	arr.assign(y, std::vector<int>(x, 0));
+	Omok::drawMap();
+}
+
+Omok::~Omok()
+{
+	arr.clear();
+}
+
+
+
+void drawTopLeft() { std::cout << "¦£"; }
+void drawTopCenter() { std::cout << "¦¨"; }
+void drawTopRight() { std::cout << "¦¤"; std::cout << "" << std::endl; }
+
+void drawLeft() { std::cout << "¦§"; }
+void drawCenter() { std::cout << "¦«"; }
+void drawRight() { std::cout << "¦©"; std::cout << "" << std::endl; }
+
+void drawBottomLeft() { std::cout << "¦¦"; }
+void drawBottom() { std::cout << "¦ª"; }
+void drawBottomRight() { std::cout << "¦¥"; }
+
+void Omok::drawMap()
+{
+	//	map.resize(x*y);
+	Omok::gotoxy(0, 0);
+	int cnt = 0;
+	for (int i = 0; i<loop_y; i++)
+	{
+		if (i == 0) drawTopLeft();
+		else if (i >= 1 && i <= y - 2) drawLeft();
+		else if (i == y - 1)  drawBottomLeft();
+		for (int j = 0; j < loop_x; j++)
+		{
+			if (i == 0 && j >= 1 && j <= x - 2) drawTopCenter();
+			else if (i == 0 && j == x - 1)  drawTopRight();
+			else if (i != 0 && i <= y - 2 && j >= 1 && j <= x - 2) drawCenter();
+			else if (i != 0 && i <= y - 2 && j == x - 1) drawRight();
+			else if (i == y - 1 && j >= 1 && j <= x - 2) drawBottom();
+			else if (i == y - 1 && j == x - 1) drawBottomRight();
+		}
+	}
+	//x = x / 2;
+	//y = y / 2;
+}
+
 bool Omok::check_win()
 {
 	int b_cnt = 0;
 	int w_cnt = 0;
 	int cnt = 1;
 
-	for (int i = 0; i < 20; i++) //length_check start
+	for (int i = 0; i < loop_y; i++) //length_check start
 	{
-		for (int j = 0; j < 20; j++)
+		for (int j = 0; j < loop_x; j++)
 		{
-			if ((arr[i][j] == 8) && (arr[i][j+1]==8) && (arr[i][j+2] ==8) && (arr[i][j+3]==8) && (arr[i][j+4]==8))
+			if ((arr[i][j] == 8) && (arr[i][j + 1] == 8) && (arr[i][j + 2] == 8) && (arr[i][j + 3] == 8) && (arr[i][j + 4] == 8))
 			{
 				return 0;
 			}
-			else if ((arr[i][j] == 9) && (arr[i][j + 1] == 9) && (arr[i][j + 2] == 9) && (arr[i][j + 3] == 9) && (arr[i][j + 4] ==9))
+			else if ((arr[i][j] == 9) && (arr[i][j + 1] == 9) && (arr[i][j + 2] == 9) && (arr[i][j + 3] == 9) && (arr[i][j + 4] == 9))
 			{
 				return 1;
 			}
 		}
 	} //length_check finish
 
-	for (int i = 0; i < 20; i++) // width_check start
+	for (int i = 0; i < loop_y; i++) // width_check start
 	{
-		for (int j = 0; j < 20; j++)
+		for (int j = 0; j < loop_x; j++)
 		{
 			if ((arr[j][i] == 8) && (arr[j + 1][i] == 8) && (arr[j + 2][i] == 8) && (arr[j + 3][i] == 8) && (arr[j + 4][i] == 8))
 			{
@@ -56,29 +108,43 @@ bool Omok::check_win()
 			}
 		}
 	} // width_check finish
-	
-	for (int i = 0; i < 20; i++) //reverse_cross_check start
+
+	for (int i = 0; i < loop_y; i++) //reverse_cross_check start
 	{
-		for (int j = 0; j < 20; j++)
+		for (int j = 0; j < loop_x; j++)
 		{
-			if ((arr[i][j] == 8) && (arr[i-1][j +1] == 8) && (arr[i-2][j + 2] == 8) && (arr[i-3][j +3] == 8) && (arr[i-4][j +4] == 8))
+			if ((arr[i][j] == 8) && (arr[i - 1][j + 1] == 8) && (arr[i - 2][j + 2] == 8) && (arr[i - 3][j + 3] == 8) && (arr[i - 4][j + 4] == 8))
 			{
 				return 0;
 			}
-			else if ((arr[i][j] == 9) && (arr[i-1][j + 1] == 9) && (arr[i-2][j + 2] == 9) && (arr[i-3][j + 3] == 9) && (arr[i-4][j + 4] == 9))
+			else if ((arr[i][j] == 9) && (arr[i - 1][j + 1] == 9) && (arr[i - 2][j + 2] == 9) && (arr[i - 3][j + 3] == 9) && (arr[i - 4][j + 4] == 9))
 			{
 				return 1;
 			}
 		}
 	} //reverse_cross_check finish
-	
+	for (int i = 0; i < loop_y; i++) //reverse_cross_check start
+	{
+		for (int j = 0; j < loop_x; j++)
+		{
+			if ((arr[i][j] == 8) && (arr[i + 1][j + 1] == 8) && (arr[i + 2][j + 2] == 8) && (arr[i + 3][j + 3] == 8) && (arr[i + 4][j + 4] == 8))
+			{
+				return 0;
+			}
+			else if ((arr[i][j] == 9) && (arr[i + 1][j + 1] == 9) && (arr[i + 2][j + 2] == 9) && (arr[i + 3][j + 3] == 9) && (arr[i + 4][j + 4] == 9))
+			{
+				return 1;
+			}
+		}
+	} //reverse_cross_check finish
+
 }
 
 bool Omok::set()
 {
 	return game;
 }
-void Omok::gotoxy(int g_x,int g_y)
+void Omok::gotoxy(int g_x, int g_y)
 {
 	COORD Pos;
 	Pos.X = g_x;
@@ -94,42 +160,42 @@ void Omok::move()
 		key = getch();
 		switch (key)
 		{
-		
+
 
 		case 75: // left move
 			if (x > 0)
-			{	
+			{
 				x -= 1;
-				gotoxy(x * 2,y);
+				gotoxy(x * 2, y);
 			}
 			break;
 		case 77: // right move 
-			if (x < 19)
+			if (x < loop_x  - 1)
 			{
 				x += 1;
-				gotoxy(x * 2,y);
+				gotoxy(x * 2, y);
 			}
 			break;
 		case 72: // up move 
 			if (y > 0)
 			{
 				y -= 1;
-				gotoxy(x*2,y);
+				gotoxy(x * 2, y);
 			}
 			break;
 
 		case 80: // down move 
-			if (y < 19)
+			if (y < loop_y - 1)
 			{
 				y += 1;
-				gotoxy(x*2,y);
+				gotoxy(x * 2, y);
 
 			}
 			break;
 		case 79: // push spacebar
 			if (arr[y][x] != 0)
 			{
-				msg(3);
+				//msg(3);
 			}
 			else if (arr[y][x] == 0)
 			{
@@ -137,23 +203,23 @@ void Omok::move()
 				{
 					arr[y][x] = 8;
 					std::cout << "¡Û"; // black
-					turn = true;	
-					gotoxy(x*2,y);
+					turn = true;
+					gotoxy(x * 2, y);
 					msg(check_win());
-					gotoxy(40,0);
-					std::cout << "Turn : ¡Ü" << std::endl;
-					gotoxy(x*2, y);
+					//gotoxy(40, 0);
+					//std::cout << "Turn : ¡Ü" << std::endl;
+					gotoxy(x * 2, y);
 				}
 				else if (turn == true)
 				{
 					arr[y][x] = 9;
 					std::cout << "¡Ü"; // whtie
 					turn = false;
-					gotoxy(x*2,y);
+					gotoxy(x * 2, y);
 					msg(check_win());
-					gotoxy(40, 0);
-					std::cout << "Turn : ¡Û" << std::endl;
-					gotoxy(x*2, y);
+					///gotoxy(40, 0);
+					///std::cout << "Turn : ¡Û" << std::endl;
+					gotoxy(x * 2, y);
 				}
 			}
 			break;
@@ -173,69 +239,19 @@ void Omok::msg(int i)
 		std::cout << " ¡Û Win!";
 		game = 0;
 	}
-	else if(i == 1)
+	else if (i == 1)
 	{
 		std::cout << " ¡Ü Win!";
 		game = 0;
 	}
-	else if (i == 3)
+	/*else if (i == 3)
 	{
 		std::cout << "You can`t locate the rock here";
 		Sleep(1000);
-		Pos.X = 0;
-		Pos.Y = 20;
+		Pos.X = x*10;
+		Pos.Y = 0;
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 		std::cout << "                              ";
-	}
+	}*/
 
-}
-void Omok::start()
-{
-	for (int i = 0; i < _Y; i++)
-	{
-		for (int j = 0; j < _X; j++)
-		{
-			switch (arr[i][j])
-			{
-			case 0:
-				std::cout << "¦«";
-				break;
-			case 1:
-				std::cout << "¦§";
-				arr[i][j] = 0;
-				break;
-			case 2:
-				std::cout << "¦©";
-				arr[i][j] = 0;
-				break;
-			case 3:
-				std::cout << "¦£";
-				arr[i][j] = 0;
-				break;
-			case 4:
-				std::cout << "¦¤";
-				arr[i][j] = 0;
-				break;
-			case 5:
-				std::cout << "¦¨";
-				arr[i][j] = 0;
-				break;
-			case 6:
-				std::cout << "¦ª";
-				arr[i][j] = 0;
-				break;
-			case 7:
-				std:: cout << "¦¦";
-				arr[i][j] = 0;
-				break;
-			case 8:
-				std::cout << "¦¥";
-				arr[i][j] = 0;
-				break;
-			}
-		}
-		std::cout << " " << std::endl;
-	}
-	gotoxy(40, 0);
-	std::cout << "Turn : ¡Û" << std::endl;
 }
